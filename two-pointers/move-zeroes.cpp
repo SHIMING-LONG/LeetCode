20cbf8d71fe281c1ce61eb60af7b0f15592e0435
SHIMING-LONG <shimingl@chalmers.se>
2026-04-18 23:50:57 +0200

    LC-move-zeroes [Runtime: 0 ms; Memory: 24 MB]

--- two-pointers/move-zeroes.cpp
+++ two-pointers/move-zeroes.cpp
@@ -4,15 +4,11 @@ public:
         int slowindex=0;
         for(int fastindex=0; fastindex<nums.size(); fastindex++){
             if (nums[fastindex]!=0){
-                
-                nums[slowindex]=nums[fastindex];
+                swap(nums[slowindex], nums[fastindex]);
                 slowindex++;
-
             }
         }
-        for (int i=slowindex; i<nums.size();i++){
-            nums[i]=0;
-        }
+      
         
     }
 };
