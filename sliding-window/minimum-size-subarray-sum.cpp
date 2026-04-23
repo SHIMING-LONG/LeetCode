efe638b33044361f10dec12a39a0e96cde8a46f3
SHIMING-LONG <shimingl@chalmers.se>
2026-04-23 17:01:32 +0200

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- sliding-window/minimum-size-subarray-sum.cpp
+++ sliding-window/minimum-size-subarray-sum.cpp
@@ -4,13 +4,14 @@ public:
         int i=0; // the start point
         int j=0; // the ending point
         int length=0; // the window length
-        int sum=nums[i]; //the sum of the window
+        int sum=0; //the sum of the window
         int result = 0; // initial
         for (;j<nums.size();j++){
             sum +=nums[j];
             while(sum>=target){ 
                 length=(j-i+1);
-                result=length;
+                if (length<result){result=length;}
+               
                 sum -=nums[i]; // the window smaller
                 i++; // start point move forward
 
