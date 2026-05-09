4490708cb62e35bcf3a4896f030bcdc80c9c789a
SHIMING-LONG <shimingl@chalmers.se>
2026-05-09 21:52:17 +0200

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

---
+++ linked-list/linked-list-cycle-ii.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ //从头结点出发一个指针，从相遇节点 也出发一个指针，这两个指针每次只走一个节点， 那么当这两个指针相遇的时候就是 环形入口的节点
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+            // 快慢指针相遇，此时从head 和 相遇点，同时查找直至相遇
+            if(slow==fast){
+                ListNode *index1=head;
+                ListNode *index2=fast;
+                while(index1!=index2){
+                    index1=index1->next;
+                    index2=index2->next;
+                }
+                return index1;
+            }
+        }
+        return nullptr;
+        
+    }
+};
