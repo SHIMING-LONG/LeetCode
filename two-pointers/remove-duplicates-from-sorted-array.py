3f09646dbde381b71229d0bb5bbbddcaa488c80f
SHIMING-LONG <shimingl@chalmers.se>
2026-06-18 14:58:50 +0800

    LC-remove-duplicates-from-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow=0
+        fast=1
+        while (fast<len(nums)):
+            if nums[slow]!=nums[fast]:
+                slow+=1
+                nums[slow]=nums[fast]
+            fast+=1
+        return slow+1
