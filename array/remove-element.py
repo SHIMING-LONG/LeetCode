dcc64f528ace9c94c13ace07d712c07f3f4c1ea1
SHIMING-LONG <shimingl@chalmers.se>
2026-06-18 19:48:03 +0800

    LC-remove-element [Runtime: N/A; Memory: N/A]

---
+++ array/remove-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        slow=0
+        fast=0
+        while (fast<len(nums)):
+            if nums[fast]!=val:
+                nums[slow]=nums[fast]
+                slow+=1
+            fast+=1
+        return slow
